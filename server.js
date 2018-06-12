dbe8c627aed9d6964053f2a61bb6774dd6cb9472
Jag Chumber <jagdeep.chumber@zonedigital.com>
2018-06-12 11:11:48 +0100

    Added user modal registration

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to DB
 mongoose
   .connect(db)
-  .then(() => console.log("db connected"))
+  .then(() => console.log('db connected'))
   .catch(err => onsole.log(err));
 
-app.get("/", (req, res) => res.send("hello!!!!"));
+app.get('/', (req, res) => res.send('hello!!!!'));
 
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
