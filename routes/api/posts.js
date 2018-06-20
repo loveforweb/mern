7b12e372559442bb312529cabb41ea025d883ca8
Jag Chumber <jag@loveforweb.com>
2018-06-20 22:06:41 +0100

    Added add and delete comment to post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -149,4 +149,67 @@ router.post(
   }
 );
 
+// @route POST api/posts/comment/:id
+// @desc Add comment to post
+// @access Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postNotFound: 'No post found' }));
+  }
+);
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Delete a comment from post
+// @access Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentNotFound: 'Comment do not exist' });
+        }
+
+        // Remove
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postNotFound: 'No post found' }));
+  }
+);
 module.exports = router;
