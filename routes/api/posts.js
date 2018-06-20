c397997028ca11f3e7123e7e0069b6c24260f403
Jag Chumber <jag@loveforweb.com>
2018-06-20 20:35:51 +0100

    Added get all pst, post by id and delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 
 // Post model
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // Load validation
 const validatePostInput = require('../../validation/post');
@@ -14,6 +15,27 @@ const validatePostInput = require('../../validation/post');
 // @desc Tests post route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'post works' }));
+
+// @route GET api/post
+// @desc Get posts
+// @access Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noPosts: 'no post found' }));
+});
+
+// @route GET api/post/:id
+// @desc Get post by id
+// @access Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ noPostWithID: 'no post found with that id' })
+    );
+});
 
 // @route POST api/post
 // @desc Create post
@@ -39,6 +61,31 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route DELETE api/post/:id
+// @desc Delete post with id
+// @access Private
+
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notAuthorised: 'User not authorised' });
+        }
+
+        post
+          .remove()
+          .then(() => res.json({ success: true }))
+          .catch(err =>
+            res.status(404).json({ postNotFound: 'Post not found' })
+          );
+      });
+    });
   }
 );
 
