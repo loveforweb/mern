0780fa70c3769104825be3ec5105cf2cd28eaa42
Jag Chumber <jagdeep.chumber@zonedigital.com>
2018-06-21 15:14:13 +0100

    Added redux login action and set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 // REGISTER
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +14,41 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/user/login', userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+
+      // Set to localStorage
+      localStorage.setItem('jwtToken', token);
+
+      // Set token to Auth header
+      setAuthToken(token);
+
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
