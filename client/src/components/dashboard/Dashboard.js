8933bc63c35f5471311d18cf542fc6d10badd308
Jag Chumber <jagdeep.chumber@zonedigital.com>
2018-06-25 16:48:05 +0100

    Updated dashboard to display and delete education

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
 import Experience from './Experience';
+import Education from './Education';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -35,6 +36,7 @@ class Dashboard extends Component {
             <ProfileActions />
 
             <Experience experience={profile.experience} />
+            <Education education={profile.education} />
 
             <div style={{ marginBottom: '60px' }} />
 
