595e1ba151df79dc29b790548cded16aff5a2411
Jag Chumber <jagdeep.chumber@zonedigital.com>
2018-06-26 09:38:09 +0100

    Added get profile by handle and sub components

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 
 import './App.css';
+import Profile from './components/profile/Profile';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -61,6 +62,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
