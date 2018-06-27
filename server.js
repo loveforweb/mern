3bdd70ce0f694dcfabebfa0bf961c6afba408b8d
Jag Chumber <jagdeep.chumber@zonedigital.com>
2018-06-27 09:01:29 +0100

    Prepared for heroku deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -26,9 +27,19 @@ mongoose
 app.use(passport.initialize());
 
 // Passport Config
-// Passport Config
 require('./config/passport.js')(passport);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// User Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
